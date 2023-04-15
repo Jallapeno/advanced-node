f8a12f2613a2c45d17c3b06982b88152ebdb56e5
Jallapeno <hytalo48@gmail.com>
2023-04-14 22:58:43 -0300

    refactor code

--- tests/infra/crypto/jwt-token-generator.spec.ts
+++ tests/infra/crypto/jwt-token-generator.spec.ts
@@ -14,24 +14,30 @@ class JwtTokenGenerator {
 describe('JwtTokenGenerator', () => {
   let sut: JwtTokenGenerator
   let fakeJwt: jest.Mocked<typeof jwt>
+  let key: string
+  let expirationInMs: number
+  let anySecret: string
 
   beforeAll(() => {
     fakeJwt = jwt as jest.Mocked<typeof jwt>
     fakeJwt.sign.mockImplementation(() => 'any_token')
+    key = 'any_key'
+    expirationInMs = 1000
+    anySecret = 'any_secret'
   })
 
   beforeEach(() => {
-    sut = new JwtTokenGenerator('any_secret')
+    sut = new JwtTokenGenerator(anySecret)
   })
 
   it('should call sign with correct params', async () => {
-    await sut.generateToken({ key: 'any_key', expirationInMs: 1000 })
+    await sut.generateToken({ key, expirationInMs })
 
-    expect(fakeJwt.sign).toHaveBeenCalledWith({ key: 'any_key' }, 'any_secret', { expiresIn: 1 })
+    expect(fakeJwt.sign).toHaveBeenCalledWith({ key }, anySecret, { expiresIn: 1 })
   })
 
   it('should return a token', async () => {
-    const token = await sut.generateToken({ key: 'any_key', expirationInMs: 1000 })
+    const token = await sut.generateToken({ key, expirationInMs })
 
     expect(token).toBe('any_token')
   })
@@ -39,7 +45,7 @@ describe('JwtTokenGenerator', () => {
   it('should rethrow if sign throws', async () => {
     fakeJwt.sign.mockImplementation(() => { throw new Error('token_error') })
 
-    const promise = sut.generateToken({ key: 'any_key', expirationInMs: 1000 })
+    const promise = sut.generateToken({ key, expirationInMs })
 
     await expect(promise).rejects.toThrow(new Error('token_error'))
   })
