27f2ddfc0cf9aaddb667d4a2d09cc25d8de3780d
Jallapeno <hytalo48@gmail.com>
2023-04-19 14:07:55 -0300

    ensure PgUserAccountRepository returns undefined if email does not exists

--- tests/infra/postgres/repos/user-account.spec.ts
+++ tests/infra/postgres/repos/user-account.spec.ts
@@ -46,15 +46,30 @@ describe('PgUserAccountRepository', () => {
       })
       // await connection.initialize()
       await connection.synchronize()
-
       const pgUserRepo = getRepository(PgUser)
       await pgUserRepo.save({ email: 'existing_email' })
-
       const sut = new PgUserAccountRepository()
 
       const account = await sut.load({ email: 'existing_email' })
 
       expect(account).toEqual({ id: '1' })
+      await connection.close()
+    })
+
+    it('should return undefined if email does not exists', async () => {
+      const db = newDb()
+      const connection = await db.adapters.createTypeormConnection({
+        type: 'postgres',
+        entities: [PgUser]
+      })
+      // await connection.initialize()
+      await connection.synchronize()
+      const sut = new PgUserAccountRepository()
+
+      const account = await sut.load({ email: 'new_email' })
+
+      expect(account).toBeUndefined()
+      await connection.close()
     })
   })
 })
