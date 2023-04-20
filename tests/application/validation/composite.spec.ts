bd11065e83315338393e822ac7d77f7050b8a442
Jallapeno <hytalo48@gmail.com>
2023-04-20 20:27:02 -0300

    ensure ValidationComposite returns the first error found

---
+++ tests/application/validation/composite.spec.ts
@@ -0,0 +1,49 @@
+import { type MockProxy, mock } from 'jest-mock-extended'
+
+interface Validator {
+  validate: () => Error | undefined
+}
+
+class ValidationComposite implements Validator {
+  constructor (private readonly validators: Validator[]) {}
+  validate (): Error | undefined {
+    for (const validator of this.validators) {
+      const error = validator.validate()
+      if (error !== undefined) {
+        return error
+      }
+    }
+  }
+}
+describe('ValidationComposite', () => {
+  let sut: ValidationComposite
+  let validator1: MockProxy<Validator>
+  let validator2: MockProxy<Validator>
+  let validators: Validator[]
+
+  beforeAll(() => {
+    validator1 = mock()
+    validator1.validate.mockReturnValue(undefined)
+    validator2 = mock()
+    validator2.validate.mockReturnValue(undefined)
+    validators = [validator1, validator2]
+  })
+
+  beforeEach(() => {
+    sut = new ValidationComposite(validators)
+  })
+  it('should return undefined if all Validators return undefined', () => {
+    const error = sut.validate()
+
+    expect(error).toBeUndefined()
+  })
+
+  it('should return the first error', () => {
+    validator1.validate.mockReturnValue(new Error('error_1'))
+    validator2.validate.mockReturnValue(new Error('error_2'))
+
+    const error = sut.validate()
+
+    expect(error).toEqual(new Error('error_1'))
+  })
+})
